cbe546bbf32558c51dc0a94ab5f3888e732ba9e4
Michael <michael@michael.com>
2021-01-03 00:56:52 +0100

    add CLI

--- README.md
+++ README.md
@@ -1,22 +1,17 @@
 # domanek_project_1
 
 ## Aufgabenstellung
-Simulation einer Datenübertragung von ganzen Zahlen basierend auf
-der Kodierung Signed LEB128 wobei Zahlen (zufällig zwischen ‐100000
-und 100000; über die Kommandozeile konfigurierbar) zwischen zwei
-Threads übertragen werden sollen. Es sollen permanent Zahlen im Se‐
-kundentakt übertragen werden, wobei die Übertragung als String statt‐
-finden soll. Es sind promise und future Paare zur Kommunikation
-zu verwenden.
+Simulation einer Datenübertragung von ganzen Zahlen basierend auf der Kodierung Signed LEB128 wobei Zahlen (zufällig zwischen ‐100000 und 100000; über die Kommandozeile konfigurierbar) zwischen zwei Threads übertragen werden sollen. 
+Es sollen permanent Zahlen im Sekundentakt übertragen werden, wobei die Übertragung als String stattfinden soll. Es sind promise und future Paare zur Kommunikation zu verwenden.
 
 
 ## Beschreibung
 
-Standartmäßig werden zufällige Zahlen zu signed LEB128 konvertiert und zwischen 2 Threads übertragen. 
+Standartmäßig werden zufällige Dezimalzahlen zu signed LEB128 konvertiert und zwischen 2 Threads übertragen und wieder zurück zu einer Dezimalzahlen konvertiert. 
 Über Kommandozeileninterface ist es möglich:
 - unsigned LEB128 zu verwenden
 - Start und Ende des Bereichs der Zufallszahlen festzulegen
-- Werte über CLI, JSON, TOML oder plain files einlesen
+- Werte über CLI einlesen und dauerhaft übertragen
 - Werte in Kommandozeile ausgeben oder in JSON, TOML, oder plain file schreiben
 - Dauer zwischen der Übertragung festlegen
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,65 @@
+#include "CLI11.hpp"
 
 #include <iostream>
 #include <thread>
+#include <vector>
+#include <exception>
+#include <string>
 
 using namespace std;
 
-//int main(int argc, char* argv[]) {
-int main() {
-    thread t{[]{ cout << "Hello"; }};
-    t.join();
-    cout << " world!" << endl;
-    return 0;
+auto throwValidationError(const CLI::App &app, const string &error) {
+    try {
+        throw CLI::ValidationError(error);
+    } catch (const CLI::Error &e) {
+        return app.exit(e);
+    }
+}
+
+
+int main(int argc, char* argv[]) {
+    CLI::App app{"Simulation of data transfer with LEB128"};
+
+    bool transferUnsigned{false};
+    app.add_flag("-u,--unsigned", transferUnsigned, "Encode with unsigned Leb128");
+
+    int start{-100000};
+    auto startOption {
+        app.add_option("-s,--start", start, "Start of the range of random numbers")
+        ->check(CLI::Range(-100000, 100000))
+    };
+
+    int end{100000};
+    auto endOption {
+        app.add_option("-e,--end", end, "End of the range of random numbers")
+        ->check(CLI::Range(-100000, 100000))
+    };
+
+    unsigned short delay{1000};
+    app.add_option("-d,--delay", delay, "Delay between data transfer in ms");
+
+    vector<int> values;
+    auto valuesOption{app.add_option("-v,--values", values, "Values to transfer in a loop")};
+
+    string jsonOutputPath;
+    app.add_option("--json-output-name", jsonOutputPath, "Name of json output file");
+
+    string tomlOutputPath;
+    app.add_option("--toml-output-name", tomlOutputPath, "Name of toml output file");
+
+    string fileOutputPath;
+    app.add_option("--file-output-name", fileOutputPath, "Name of plain output file");
+
+    startOption->excludes(valuesOption);
+    startOption->needs(endOption);
+    endOption->needs(startOption);
+
+    CLI11_PARSE(app, argc, argv);
+
+    if(transferUnsigned and start < 0) {
+        return throwValidationError(app, "--start: start must be between 0 and 100000");
+    } else if (start > end) {
+        return throwValidationError(app, "--start --end: start must be smaller than end");
+    }
+    
 }
